6ebb0188425426fb2841a3b8f3c2ea29d7f71f2d
Luan Rodrigues <luanzragit@gmail.com>
2025-10-08 20:12:05 -0300

    condicionais js

---
+++ array-methods/js/scriptCondicional.js
@@ -0,0 +1,38 @@
+/*
+let lampada = "off";
+
+if (lampada == "on") {
+    document.getElementById("altere").innerHTML = "A lampada está ligada";
+} else {
+    document.getElementById("altere").innerHTML = "A lampada está está desligada";
+}
+*/
+
+/*
+var hora = new Date().getHours();
+
+if (hora < 12) {
+    alert("Bom dia!")
+} else if (hora < 18) {
+    alert("Boa tarde!")
+} else {
+    alert("Boa Noite!")
+}
+*/
+
+/*
+function verificar() {
+    let nome = document.getElementById("nome").value;
+
+    if (nome == "" || nome == null) {
+        let verifica = document.getElementById("altere")
+        verifica.innerHTML = "O campo não pode ser vazio";
+        verifica.style.color = "red";
+    } else {
+        let verifica = document.getElementById("altere")
+        verifica.innerHTML = "Nome cadastrado com sucesso!"
+        verifica.style.color = "green"
+    }
+}
+*/
+
